87f9b24816d96e61faf7c2ebe0b517e21449b213
wjs0521 <wjs0521@gmail.com>
2017-05-07 09:16:50 +0800

    MVC Will Day 1

--- MVC5Course/Controllers/ProductsController.cs
+++ MVC5Course/Controllers/ProductsController.cs
@@ -7,6 +7,7 @@ using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using MVC5Course.Models;
+using MVC5Course.Models.ViewModels;
 
 namespace MVC5Course.Controllers
 {
@@ -15,9 +16,13 @@ namespace MVC5Course.Controllers
         private FabricsEntities db = new FabricsEntities();
 
         // GET: Products
-        public ActionResult Index()
+        public ActionResult Index(bool Active = true)
         {
-            return View(db.Product.OrderByDescending(p => p.ProductId).Take(10));
+            var data = db.Product
+                .Where(p => p.Active.HasValue && p.Active == Active)
+                .OrderByDescending(p => p.ProductId)
+                .Take(10);
+            return View(data);
         }
 
         // GET: Products/Details/5
@@ -122,6 +127,20 @@ namespace MVC5Course.Controllers
                 db.Dispose();
             }
             base.Dispose(disposing);
+        }
+
+        public ActionResult ListProducts()
+        {
+            var data = db.Product
+                .Where(p => p.Active == true)
+                .Select(p => new ProductLiteVM {
+                    ProductId = p.ProductId,
+                    ProductName = p.ProductName,
+                    Price = p.Price,
+                    Stock = p.Stock
+                })
+                .Take(10);
+            return View(data);
         }
     }
 }
---
+++ MVC5Course/Models/Product.Partial.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVC5Course.Models
+{
+    [MetadataType(typeof(ProductMetaData))]
+    public partial class Product
+    {
+
+    }
+
+    public partial class ProductMetaData
+	{
+        public int ProductId { get; set; }
+        [Required(ErrorMessage = "請輸入商品名稱")]
+        [MinLength(3), MaxLength(30)]
+        [RegularExpression("(.+)-(.+)", ErrorMessage = "商品名稱格式錯誤")]
+        [DisplayName("商品名稱")]
+        public string ProductName { get; set; }
+        [Required]
+        [Range(0, 9999, ErrorMessage = "請設定正確的商品價格範圍")]
+        [DisplayFormat(DataFormatString = "{0:0}", ApplyFormatInEditMode = true)]
+        [DisplayName("商品價格")]
+        public Nullable<decimal> Price { get; set; }
+        [Required]
+        [DisplayName("是否上架")]
+        public Nullable<bool> Active { get; set; }
+        [Required]
+        [Range(0, 100, ErrorMessage = "請設定正確的商品價格範圍")]
+        [DisplayName("商品庫存")]
+        public Nullable<decimal> Stock { get; set; }
+    }
+}
